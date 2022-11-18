816cfe9c561bd90903b1ec00b0f4ea6a3f1eba48
siuyin <siuyin@beyondbroadcast.com>
2022-11-18 18:28:25 +0800

    Update make_handout.sh

--- make_handout.sh
+++ make_handout.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 handout_print.sh $1.pdf 
 pdf270 cover_page.pdf
-pdfjam --suffix combined --landscape cover_page-rotated270.pdf '1,{}' $1-2x2.pdf
 
+# enter `handout_print.sh ... 2` for double sided print pdf
+if [ "$2" = "2" ]; then
+        pdfjam --suffix combined --landscape cover_page-rotated270.pdf '1,{}' $1-2x2.pdf
+else
+        pdfjam --suffix combined --landscape cover_page-rotated270.pdf $1-2x2.pdf
+fi
